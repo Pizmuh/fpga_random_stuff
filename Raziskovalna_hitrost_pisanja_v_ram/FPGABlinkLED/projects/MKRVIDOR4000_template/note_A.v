2b9c3b9e210d9b6efcb87c41ec28ca6445ee7983
Pizmuh <simon.lipovsek@gmail.com>
2023-06-15 18:02:31 +0200

    shader branch

--- Raziskovalna_hitrost_pisanja_v_ram/FPGABlinkLED/projects/MKRVIDOR4000_template/note_A.v
+++
@@ -1,9 +0,0 @@
-module note_A
-(
-	input clk,
-	output speaker
-);
-	reg[15:0] counter;
-	always @(posedge clk) if(counter==56817) counter <= 0; else counter <=counter+1;
-	assign speaker = counter[15];
-endmodule
